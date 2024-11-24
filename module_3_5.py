c9e4af5c0d7e35f168247f3191efe6c2e2e29988
Coolnezz03 <121030949+Coolnezz03@users.noreply.github.com>
2024-11-24 14:59:07 +0800

    Update module_3_5.py

--- module_3_5.py
+++ module_3_5.py
@@ -1,12 +1,12 @@
 def get_multiplied_digits(number):
     str_number = str(number)
-    if len(str_number) > 1 and str_number != '0':
+    if len(str_number) > 1:
         first = int(str_number[0])
         return first * get_multiplied_digits(int(str_number[1:]))
     elif str_number == '0':
         return 1
     else:
-        return first
+        return int(str_number[0])
 
 
 
